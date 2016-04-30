3215d4d6d14c5c67fe4bff92a74cea3e07caba3f
Casey Gollan <hello@caseyagollan.com>
2016-04-30 13:33:03 -0400

    fix music page

--- site/templates/music.php
+++ site/templates/music.php
@@ -13,8 +13,10 @@
     <?php if (!$image->link()->empty()): ?><a href="<?php echo $image->link() ?>"><?php endif ?>
       <img src="<?php echo thumb($image, array('width' => 400))->url(); ?>" alt="<?php echo $page->title()->html() ?>">
     <?php if (!$image->link()->empty()): ?></a><?php endif ?>
-    <?php if (!$image->itunes()->empty()): ?></p><a href="<?php echo $image->itunes() ?>"><i class="fa fa-lg fa-apple"></i> Apple Music</a> &nbsp;<?php endif ?>
-      <?php if (!$image->spotify()->empty()): ?><a href="<?php echo $image->spotify() ?>"><i class="fa fa-lg fa-spotify"></i> Spotify</a><?php endif ?>
+    <p class="store-links">
+      <?php if (!$image->itunes()->empty()): ?><a href="<?php echo $image->itunes() ?>"><i class="fa fa-lg fa-apple"></i> Apple Music</a> &nbsp;<?php endif ?>
+      <?php if (!$image->spotify()->empty()): ?><a href="<?php echo $image->spotify() ?>"><i class="fa fa-lg fa-spotify"></i> Spotify</a> &nbsp;<?php endif ?>      
+    </p>
     </div>
   <?php endforeach ?>
 </section>
