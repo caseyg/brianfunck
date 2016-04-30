bf0b01c1d167d6de5bc60dcb946c3ceade4a2f10
Casey Gollan <hello@caseyagollan.com>
2016-04-30 12:48:02 -0400

    simplify company festival image controller

--- site/controllers/project.php
+++ site/controllers/project.php
@@ -4,8 +4,8 @@ return function($site, $pages, $page) {
 
   $poster = $page->images()->filterBy('filename', '*=', 'poster')->first();
   $background = $page->images()->filterBy('filename', '*=', 'background')->first();
-  $company = $page->images()->filterBy('filename', '*=', 'company');
-  $festival = $page->images()->filterBy('filename', '*=', 'festival');
+  $company = $page->images()->filterBy('filename', '*=', 'company')->first();
+  $festival = $page->images()->filterBy('filename', '*=', 'festival')->first();
 
   return compact('poster', 'background', 'company', 'festival');
 
